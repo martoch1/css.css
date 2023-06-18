3b28328e915aaea6cb5d83386db2e56ab41a5d17
martoch1 <martinsochei@gmail.com>
2023-06-18 06:44:35 -0700

    initial commit

---
+++ exercise.js/exercise.js
@@ -0,0 +1,43 @@
+// console.log();
+// alert("10 + 5")
+// alert("10 - 2")
+// console.log(alert);
+// alert(15 % 9);
+// let x = 10;
+// let y = 5;
+// x += y;
+// alert(x);
+// let x = 10;
+// let y = 40;
+// x += y;
+// alert(x);
+// x = 10;
+// y = 5;
+// alert(x > y);
+//  x = 10;
+//  y = 10;
+//  alert(x == y);
+// • Choose the correct comparison operator to alert true, when x is NOT equal to y.
+//  x = 10;
+//  y = 5;
+//  alert(x < y);
+//  Write a program to alert "Hello World" if x is greater than y.
+// x = 10;
+//  y = 5;
+//  if (x > y) {
+//     alert("Hello World");
+//  }
+// x = 10;
+//  y = 5;
+//  if (x < y) {
+//       alert("i am still here");
+//   } else{
+//     alert("Goodbye");
+//   }
+let lap1 = go
+let lap2 = my
+let lap3 = code
+function laptime(){
+    lap1 + lap2 + lap3
+}
+console.log(laptime);
---
+++ main/main.js
@@ -0,0 +1,148 @@
+// console.log(999)
+
+
+
+// function lion() {
+// alert ('Martins ochei')
+// confirm ('Are you sure you want to come over')
+// prompt ('Are you sure you want to have s**')
+// }
+
+// function names(x){
+//     if (x <= 20){
+//         console.log("peter is a name");
+//     } 
+//     else if(x >= 20){
+//         console.log("peter is a name");
+//     }
+//     else{
+//         console.log("peter is not a name");
+//     }   
+// }
+// names(22)
+
+// function names(x) {
+// switch (x) {
+//     case 1:
+//         console.log("peter is not a name");
+//         break;
+//     case 2:
+//         console.log("peter is not a name");
+//         break;
+//     case 3:
+//         console.log("peter is a name");
+//         break;
+//     case 4:
+//         console.log("peter is not a name");
+//         break;
+//     default:
+//     console.log("peter is a name");
+// }    
+// }
+
+// names(3)
+// function checkpoint6(){
+//     console.log("peterpark is a name");
+// }
+
+// function checkpoint1() {
+//     console.log("peter is a name")
+// }
+
+// function checkpoint2() {
+//     console.log("peter is a name")
+// }
+// function checkpoint3() {
+// console.log("martins is a name");    
+// }
+// function checkpoint4() {
+//     console.log("blue is not a name");    
+// }
+
+// function checkpoint5() {
+//     console.log("yellow is not a name");    
+// }
+
+// function names(x) {
+//         switch (x){
+//         case 1:
+//               checkpoint1();
+//             break;
+//         case 2:
+//              checkpoint2();
+//           break;
+//         case 3:
+//              checkpoint3();
+//           break;
+//         case 4:
+//              checkpoint4();
+//           break;
+//         case 5:
+//             checkpoint5();
+//           break;
+//         case 6:
+//             checkpoint6();
+//           break;
+//         default:
+//            console.log("any");
+//         }    
+//         }    
+//         names ()
+
+
+
+// function names(x) {
+//     switch (x) {
+//         case 1:
+//             checkpoint(1);
+//             break;
+//         case 2:
+//             checkpoint(2);
+//             break;
+//         case 3:
+//             checkpoint(3);
+//             break;
+//         case 4:
+//             checkpoint(4);
+//             break;
+//         default:
+//         console.log(any);
+//     }    
+//     }
+    
+//     names(3)
+//     whileloop
+// let age = [19, 65, 1, 66, 1, 30, 52,10]
+// let sum = 4
+// i = 3
+
+// while (i < age.length) {
+//   sum += age[i]
+//     i += 1
+//   }
+//   console.log(`the loop was executed ${i} times`);
+//   console.log(sum);
+
+
+
+//   let school = "go my code"
+//   let text = "is a tech school"
+//   let wholetext = `do you know ${school} and ${text}
+//   `
+//   console.log(wholetext)
+
+
+// let num = [3,5, 23, 10, 10, 10, 10, 10, 10, 10, 10]
+// let sum = 34
+
+// for (let i = 0; i < num.length; i++) {
+//     sum += num[i]
+// }
+// console.log(sum);
+
+// let numbers = [19, 25, 28, 29, 30, 31, 32, 33, 34, 35]
+// let sum = 0;
+// for (let i in numbers){
+//   sum += numbers[i];
+// }
+// console.log(sum);
